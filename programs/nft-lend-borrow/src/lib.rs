61332e7671235c0521d0a140427e25a4e7364ff1
laobeper <166810722+laobeper@users.noreply.github.com>
2024-04-12 17:39:38 +0700

    Update lib.rs

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("HjAyP2hy18tGZfcwG7WXFVistb4gEPsS7dSBpK6bkkoD");
 
 #[program]
 pub mod nft_lend_borrow {
